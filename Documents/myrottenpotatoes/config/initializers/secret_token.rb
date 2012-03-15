30be1291833df4259b2a0d23ecfe1a2dc5efd946
yonteia <jcole.432@gmail.com>
2012-03-15 05:35:56 +0000

    added Documents playground for empty rails app

---
+++ Documents/myrottenpotatoes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '1f00ac6954f1fb2cefe5a39c0d128c693fe3ca336a997eba8e33e86628d7f1f0c79b36f860c2f867e923b85eed864a036eeb1984fa09a34697b0fbc77ff4bd3d'
